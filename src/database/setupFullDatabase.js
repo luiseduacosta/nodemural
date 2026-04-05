a2aa1ddde7af1277d3d8245e2615509f3aded364
Luis Acosta <luiseduacosta@gmail.com>
2026-04-05 19:34:19 -0300

    Added a script to update phone numbers.

--- src/database/setupFullDatabase.js
+++ src/database/setupFullDatabase.js
@@ -8,79 +8,28 @@ async function setupDatabase() {
         console.log('🚀 Starting database setup...');
 
         const queries = [
-            // 1. auth_users (users of the system. Can be admin, supervisor, professor or aluno)
-            `CREATE TABLE IF NOT EXISTS auth_users (
+
+            // 1. configuracoes (configuration)
+            `CREATE TABLE IF NOT EXISTS configuracoes (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                email VARCHAR(255) NOT NULL UNIQUE,
-                password VARCHAR(255) NOT NULL,
-                nome VARCHAR(255) NOT NULL,
-                identificacao VARCHAR(50),
-                role ENUM('admin', 'supervisor', 'professor', 'aluno') DEFAULT 'aluno',
-                entidade_id INT,
-                ativo BOOLEAN DEFAULT TRUE,
-                criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                instituicao varchar(255) NOT NULL DEFAULT 'ESS/UFRJ',
+                mural_periodo_atual CHAR(6) NOT NULL,
+                curso_turma_atual SMALLINT NOT NULL,
+                curso_abertura_inscricoes DATE,
+                curso_encerramento_inscricoes DATE,
+                termo_compromisso_periodo CHAR(6) NOT NULL,
+                termo_compromisso_inicio DATE,
+                termo_compromisso_final DATE,
+                periodo_calendario_academico CHAR(6) NOT NULL
             )`,
 
-            // 2. alunos (students of the university)
-            `CREATE TABLE IF NOT EXISTS alunos (
+            // 2. areas (areas of the institutions)
+            `CREATE TABLE IF NOT EXISTS areas (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                nome VARCHAR(255) NOT NULL,
-                nomesocial VARCHAR(255),
-                ingresso VARCHAR(6),
-                turno_id INT NULL,
-                registro VARCHAR(9) UNIQUE,
-                telefone VARCHAR(15),
-                celular VARCHAR(15),
-                email VARCHAR(255),
-                cpf VARCHAR(14),
-                identidade VARCHAR(15),
-                orgao VARCHAR(30),
-                nascimento DATE,
-                cep VARCHAR(9),
-                endereco VARCHAR(50),
-                municipio VARCHAR(30),
-                bairro VARCHAR(30),
-                observacoes TEXT
+                area VARCHAR(90) NOT NULL
             )`,
 
-            // 3. professores (university professors)
-            `CREATE TABLE IF NOT EXISTS professores (
-                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                nome VARCHAR(255) NOT NULL,
-                cpf VARCHAR(14),
-                siape VARCHAR(8) UNIQUE COMMENT 'SIAPE ID = 8 characters of numbers',
-                cress VARCHAR(10) UNIQUE,
-                regiao VARCHAR(2),
-                telefone VARCHAR(20),
-                celular VARCHAR(20),
-                email VARCHAR(255),
-                curriculolattes VARCHAR(16) COMMENT 'Lattes ID = 16 characters of numbers',
-                atualizacaolattes DATE,
-                dataingresso DATE,
-                departamento VARCHAR(30),
-                dataegresso DATE,
-                motivoegresso TEXT,
-                observacoes TEXT
-            )`,
-
-            // 4. supervisores (supervisors of the students at the institutions)
-            `CREATE TABLE IF NOT EXISTS supervisores (
-                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                nome VARCHAR(255) NOT NULL,
-                cpf VARCHAR(14) NULL,
-                email VARCHAR(255),
-                telefone VARCHAR(20) COMMENT 'Deprecated: Phone number',
-                celular VARCHAR(20),
-                cress VARCHAR(10) UNIQUE,
-                regiao VARCHAR(2) NULL,
-                escola varchar(255) NULL, 
-                ano_formacao INT NULL,
-                cargo VARCHAR(30) NULL,
-                observacoes TEXT
-            )`,
-
-            // 5. instituicoes (Institutions, places where the students can do the internship)
+            // 3. instituicoes (Institutions, places where the students can do the internship)
             `CREATE TABLE IF NOT EXISTS instituicoes (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 instituicao VARCHAR(255) NOT NULL,
@@ -102,13 +51,18 @@ async function setupDatabase() {
                 observacoes TEXT
             )`,
 
-            // 6. areas (areas of the institutions)
-            `CREATE TABLE IF NOT EXISTS areas (
+            // 5. visitas (visit to the institution to evaluate if can receive interns)
+            `CREATE TABLE IF NOT EXISTS visitas (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                area VARCHAR(90) NOT NULL
+                instituicao_id INT NOT NULL,
+                data DATE NOT NULL,
+                responsavel VARCHAR(255) NOT NULL,
+                motivo TEXT NOT NULL,
+                avaliacao TEXT NOT NULL,
+                descricao TEXT NOT NULL
             )`,
 
-            // 7. mural_estagio (internship board)
+            // 6. mural_estagio (internship board)
             `CREATE TABLE IF NOT EXISTS mural_estagios (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 instituicao_id INT NOT NULL,
@@ -132,7 +86,49 @@ async function setupDatabase() {
                 outras TEXT
             )`,
 
-            // 8. inscricoes (registrations of interns to the internship board)
+            // 7. auth_users (users of the system. Can be admin, supervisor, professor or aluno)
+            `CREATE TABLE IF NOT EXISTS auth_users (
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                nome VARCHAR(255) NOT NULL,
+                identificacao VARCHAR(50),
+                role ENUM('admin', 'supervisor', 'professor', 'aluno') DEFAULT 'aluno',
+                entidade_id INT,
+                ativo BOOLEAN DEFAULT TRUE,
+                criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                atualizado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            )`,
+
+            // 8. Turno (turn of the student)
+            `CREATE TABLE IF NOT EXISTS turnos (
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                turno VARCHAR(10) NOT NULL
+            )`
+
+            // 9. alunos (students of the university)
+            `CREATE TABLE IF NOT EXISTS alunos (
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                nome VARCHAR(255) NOT NULL,
+                nomesocial VARCHAR(255),
+                ingresso VARCHAR(6),
+                turno_id INT NULL,
+                registro VARCHAR(9) UNIQUE,
+                telefone VARCHAR(15),
+                celular VARCHAR(15),
+                email VARCHAR(255),
+                cpf VARCHAR(14),
+                identidade VARCHAR(15),
+                orgao VARCHAR(30),
+                nascimento DATE,
+                cep VARCHAR(9),
+                endereco VARCHAR(50),
+                municipio VARCHAR(30),
+                bairro VARCHAR(30),
+                observacoes TEXT
+            )`,
+
+            // 10. inscricoes (registrations of interns to the internship board)
             `CREATE TABLE IF NOT EXISTS inscricoes (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 registro VARCHAR(9) NOT NULL,
@@ -143,13 +139,50 @@ async function setupDatabase() {
                 periodo VARCHAR(6) NOT NULL
             )`,
 
-            // 9. complementos (special academic periods; estagiarios.complemento_id)
-            `CREATE TABLE IF NOT EXISTS complementos (
+            // 11. professores (university professors)
+            `CREATE TABLE IF NOT EXISTS professores (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                periodo_especial VARCHAR(255) NOT NULL
+                nome VARCHAR(255) NOT NULL,
+                cpf VARCHAR(14),
+                siape VARCHAR(8) UNIQUE COMMENT 'SIAPE ID = 8 characters of numbers',
+                cress VARCHAR(10) UNIQUE,
+                regiao VARCHAR(2),
+                telefone VARCHAR(20),
+                celular VARCHAR(20),
+                email VARCHAR(255),
+                curriculolattes VARCHAR(16) COMMENT 'Lattes ID = 16 characters of numbers',
+                atualizacaolattes DATE,
+                dataingresso DATE,
+                departamento VARCHAR(30),
+                dataegresso DATE,
+                motivoegresso TEXT,
+                observacoes TEXT
             )`,
 
-            // 10. estagiarios (students for each internship. Each student can have multiple internships)
+            // 12. supervisores (supervisors of the students at the institutions)
+            `CREATE TABLE IF NOT EXISTS supervisores (
+                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+                nome VARCHAR(255) NOT NULL,
+                cpf VARCHAR(14) NULL,
+                email VARCHAR(255),
+                telefone VARCHAR(20) COMMENT 'Deprecated: Phone number',
+                celular VARCHAR(20),
+                cress VARCHAR(10) UNIQUE,
+                regiao VARCHAR(2) NULL,
+                escola varchar(255) NULL, 
+                ano_formacao INT NULL,
+                cargo VARCHAR(30) NULL,
+                observacoes TEXT
+            )`,
+
+            // 13. inst_super (Relationship Many-to-Many between institutions and supervisors)
+            `CREATE TABLE IF NOT EXISTS inst_super (
+                supervisor_id INT NOT NULL,
+                instituicao_id INT NOT NULL,
+                PRIMARY KEY (supervisor_id, instituicao_id)
+            )`,
+
+            // 14. estagiarios (students for each internship. Each student can have multiple internships)
             `CREATE TABLE IF NOT EXISTS estagiarios (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 aluno_id INT NOT NULL,
@@ -171,13 +204,13 @@ async function setupDatabase() {
                 observacoes TEXT
             )`,
 
-            // 11. turma_estagios (groups of students for each professor by period)
+            // 15. turma_estagios (groups of students for each professor by period)
             `CREATE TABLE IF NOT EXISTS turma_estagios (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                area VARCHAR(30) NOT NULL
+                area VARCHAR(30) NOT NULL COMMENT 'Change area to turma',
             )`,
 
-            // 12. folhadeatividades (internship activity sheet fill by the intern)
+            // 16. folhadeatividades (internship activity sheet fill by the intern)
             `CREATE TABLE IF NOT EXISTS folhadeatividades (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 estagiario_id INT NOT NULL,
@@ -189,7 +222,7 @@ async function setupDatabase() {
                 periodo VARCHAR(6) NOT NULL
             )`,
 
-            // 13. questionarios (questionnaires to evaluate the interns by the supervisor)
+            // 17. questionarios (questionnaires to evaluate the interns by the supervisor)
             `CREATE TABLE IF NOT EXISTS questionarios (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 title VARCHAR(255) NOT NULL,
@@ -201,7 +234,7 @@ async function setupDatabase() {
                 target_user_type VARCHAR(50)
             )`,
 
-            // 14. questoes (questions to evaluate the interns by the supervisor)
+            // 18. questoes (questions to evaluate the interns by the supervisor)
             `CREATE TABLE IF NOT EXISTS questoes (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 questionario_id INT,
@@ -213,7 +246,7 @@ async function setupDatabase() {
                 modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             )`,
 
-            // 15. respostas (responses are the answers of the supervisor to the questions: what the supervisor evaluates about the intern)
+            // 19. respostas (responses are the answers of the supervisor to the questions: what the supervisor evaluates about the intern)
             `CREATE TABLE IF NOT EXISTS respostas (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                 questionario_id INT NOT NULL,
@@ -223,43 +256,12 @@ async function setupDatabase() {
                 modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
             )`,
 
-            // 16. visitas (visit to the institution to evaluate if can receive interns)
-            `CREATE TABLE IF NOT EXISTS visitas (
+            // 20. complementos (special academic periods; estagiarios.complemento_id)
+            `CREATE TABLE IF NOT EXISTS complementos (
                 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                instituicao_id INT NOT NULL,
-                data DATE NOT NULL,
-                responsavel VARCHAR(255) NOT NULL,
-                motivo TEXT NOT NULL,
-                avaliacao TEXT NOT NULL,
-                descricao TEXT NOT NULL
-            )`,
-
-            // 17. configuracoes (configuration)
-            `CREATE TABLE IF NOT EXISTS configuracoes (
-                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                instituicao varchar(255) NOT NULL DEFAULT 'ESS/UFRJ',
-                mural_periodo_atual CHAR(6) NOT NULL,
-                curso_turma_atual SMALLINT NOT NULL,
-                curso_abertura_inscricoes DATE,
-                curso_encerramento_inscricoes DATE,
-                termo_compromisso_periodo CHAR(6) NOT NULL,
-                termo_compromisso_inicio DATE,
-                termo_compromisso_final DATE,
-                periodo_calendario_academico CHAR(6) NOT NULL
-            )`,
-
-            // 18. inst_super (Relationship Many-to-Many between institutions and supervisors)
-            `CREATE TABLE IF NOT EXISTS inst_super (
-                supervisor_id INT NOT NULL,
-                instituicao_id INT NOT NULL,
-                PRIMARY KEY (supervisor_id, instituicao_id)
-            )`,
-
-            // 19. Turno (turn of the internship)
-            `CREATE TABLE IF NOT EXISTS turnos (
-                id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-                turno VARCHAR(10) NOT NULL
+                periodo_especial VARCHAR(255) NOT NULL
             )`
+
         ];
 
         for (const query of queries) {
